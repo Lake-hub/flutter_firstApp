78d763845aa9fdf91bccb937c397a6908a05dfba
Lake <rojas.luissm@gmail.com>
2020-04-12 15:52:34 -0500

    pantalla detalle 99%

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,35 @@ class BluePainter extends CustomPainter {
   }
 }
 
+/*Clase que dibuja las puntas en sigzag en la etiqueta de precio*/
+class DieCuttingPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+    paint.color = Colors.white;
+    paint.strokeWidth = 20;
+    paint.style = PaintingStyle.fill;
+    
+    var x = size.height/6;
+
+    var path = Path();
+    path.relativeLineTo(0, -2*x);
+
+    for (var i = 0; i < 5; i++) {
+      path.relativeLineTo(x, x);
+      path.relativeLineTo(-x, x);
+    }
+
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+  
+}
 /*Clase principal que dibuja la pantalla y contiene a los widget*/
 class SkatePage extends StatelessWidget {
   @override
@@ -50,8 +79,8 @@ class SkatePage extends StatelessWidget {
           bottom: false,
           child: Column(
             children: <Widget>[
-              appBar(),
-              content(),
+              appBar(context),
+              content(context),
               button()
             ],
           ), 
@@ -62,49 +91,123 @@ class SkatePage extends StatelessWidget {
 }
 
 //WIDGETS que serán definidos y serán usados en el SkatePage
-Widget appBar() {
-  return Container(
-    height: 150,
-    child: Row(
-
-    ),
+Widget appBar(BuildContext context) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      MaterialButton(
+        onPressed: (){
+        },
+        child: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+          size: 40.0,
+        ),
+      ),
+      SizedBox(width: 48,),
+      Flexible(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            SizedBox(height: 8,),
+            Text(
+              "SLIME MONSTER",
+              style: Theme.of(context).textTheme.title.copyWith(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                letterSpacing: 14,
+              ),
+            ),
+          ],
+        )
+      ),
+    ],
   );
 }
 
-Widget content(){
+Widget content(BuildContext context){
   return Expanded(
-    child: Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: Row(
-        children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: 140,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: ExactAssetImage('assets/board1.png'),
-                fit: BoxFit.contain,
+    child: Container(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 32.0, top: 32.0, bottom: 32.0 ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              width: 140,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: ExactAssetImage('assets/board1.png'),
+                  fit: BoxFit.contain,
+                ),
               ),
             ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 38.0, top: 32.0, bottom: 32.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('descripción de la tabla bla bla bla'),
-                  Divider(),
-                  Text('SIZE'),
-                  Text('8X32'),
-                  Text('MATERIAL'),
-                  Text('Canadian Maple'),
-                  Text('\$240'),
-                ],
-              ),
-            ) 
-          )
-        ],
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 38.0, top: 32.0, bottom: 32.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 60.0),
+                      child: Text('Handmade skateboard desk with original painting from México', 
+                        style: Theme.of(context).textTheme.title.copyWith(
+                          fontSize: 22.0,)),
+                    ),
+                    Expanded(
+                      child: Container()),
+                    Divider(),
+                    Expanded(
+                      child: Container()),
+                    Text('S I Z E', 
+                      style: Theme.of(context).textTheme.caption.copyWith(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    SizedBox(height: 10.0,),
+                    Text('8"X32"',
+                      style: Theme.of(context).textTheme.title.copyWith(
+                        fontSize: 22.0
+                      ),),
+                    Expanded(
+                      child: Container()),
+                    Text('M A T E R I A L',
+                      style: Theme.of(context).textTheme.caption.copyWith(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    SizedBox(height: 10.0,),
+                    Text('Canadian Maple',
+                      style: Theme.of(context).textTheme.title.copyWith(
+                        fontSize: 22.0
+                      ),),
+                    Expanded(
+                      flex: 3,
+                      child: Container()),
+                    Container(
+                      width: double.infinity,
+                      height: 48,
+                      color: Color(0xFFF1C929) ,
+                      alignment: Alignment.centerLeft,
+                      child: CustomPaint(
+                        painter: DieCuttingPainter(),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child:Text('\$240',
+                            style: Theme.of(context).textTheme.headline.copyWith(
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 10
+                            ),
+                          ),
+                        ),
+                      ), 
+                    ),
+                  ],
+                ),
+              ) 
+            )
+          ],
+        ),
       ),
     ),
   );
@@ -123,7 +226,7 @@ Widget button(){
     ),
     child: FlatButton(
       onPressed: () {
-        print('AGREGÓ AL CARRO');
+        print('HIZO CLICK!!');
       },
       child: Text('AGREGAR AL CARRO',
         style: TextStyle(
